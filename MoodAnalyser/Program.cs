ee25793399db87068a8582bb67bbe1451c695e0b
Apoorva1217 <apoorvarasal12@gmail.com>
2020-10-29 20:21:56 +0530

    [Apoorva] Add . Welcome to Mood Analyser Program message

---
+++ MoodAnalyser/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace MoodAnalyser
+{
+    class Program
+    {
+        /// <summary>
+        /// Mood Analyser Program
+        /// </summary>
+        /// <param name="args"></param>
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Mood Analyser Program!");
+        }
+    }
+}
