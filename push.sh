bcd4fcdf2d9d45a1e48b6c8c1982109e62fd05c0
Apoorva1217 <62049073+Apoorva1217@users.noreply.github.com>
2020-10-29 22:12:05 +0530

    Delete push.sh

--- push.sh
+++
@@ -1,9 +0,0 @@
-read a
-git branch $a
-git checkout $a
-git add .
-git commit -m "[Apoorva] Add . Handle Exception if User Provides Invalid Mood, like NULL"
-git push origin $a
-git checkout master
-git merge $a
-git push origin master --force
