243df72ce7852fb39d659835dc6879d12e34b83f
Apoorva1217 <62049073+Apoorva1217@users.noreply.github.com>
2020-10-29 21:12:41 +0530

    Delete push.sh

--- push.sh
+++
@@ -1,9 +0,0 @@
-read a
-git branch $a
-git checkout $a
-git add .
-git commit -m "[Apoorva] Add . Given a message, Ability to analyse and respond Happy or Sad Mood"
-git push origin $a
-git checkout master
-git merge $a
-git push origin master --force
