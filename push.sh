3c3c88f3cb532f24eedb9032427b2c6c4fcef0f9
Apoorva1217 <62049073+Apoorva1217@users.noreply.github.com>
2020-10-30 20:00:08 +0530

    Delete push.sh

--- push.sh
+++
@@ -1,9 +0,0 @@
-read a
-git branch $a
-git checkout $a
-git add .
-git commit -m "[Apoorva] Add . Reflection to Create MoodAnalyserMain with default constructor"
-git push origin $a
-git checkout master
-git merge $a
-git push origin master --force
