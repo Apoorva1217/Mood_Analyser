f1233e22ebfc9373c10737d49fcbcc7eabab6c14
Apoorva1217 <62049073+Apoorva1217@users.noreply.github.com>
2020-10-30 13:45:57 +0530

    Delete push.sh

--- push.sh
+++
@@ -1,9 +0,0 @@
-read a
-git branch $a
-git checkout $a
-git add .
-git commit -m "[Apoorva] Add . Inform user if enetred Invalid Mood, in case of NULL or Empty throw Exception"
-git push origin $a
-git checkout master
-git merge $a
-git push origin master --force
