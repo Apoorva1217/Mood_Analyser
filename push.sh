ae6db38f99f06c972b08ebd3f011d9fdf873849c
Apoorva1217 <62049073+Apoorva1217@users.noreply.github.com>
2020-10-31 18:56:42 +0530

    Delete push.sh

--- push.sh
+++
@@ -1,9 +0,0 @@
-read a
-git branch $a
-git checkout $a
-git add .
-git commit -m "[Apoorva] Add . Use Reflection to create Mood Analyser with Parameter Constructor"
-git push origin $a
-git checkout master
-git merge $a
-git push origin master --force
