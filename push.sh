9405e027a6c54a94d40eee9355c2dbce4d61e9f6
Apoorva1217 <62049073+Apoorva1217@users.noreply.github.com>
2020-11-01 22:48:18 +0530

    Delete push.sh

--- push.sh
+++
@@ -1,9 +0,0 @@
-read a
-git branch $a
-git checkout $a
-git add .
-git commit -m "[Apoorva] Add . Use Reflection to change mood dynamically"
-git push origin $a
-git checkout master
-git merge $a
-git push origin master --force
