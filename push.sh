b86d25d7e93be037c11ba698c2e001a4aa7e4d02
Apoorva1217 <62049073+Apoorva1217@users.noreply.github.com>
2020-11-01 22:10:35 +0530

    Delete push.sh

--- push.sh
+++
@@ -1,9 +0,0 @@
-read a
-git branch $a
-git checkout $a
-git add .
-git commit -m "[Apoorva] Add . Use Reflection to Invoke Method"
-git push origin $a
-git checkout master
-git merge $a
-git push origin master --force
